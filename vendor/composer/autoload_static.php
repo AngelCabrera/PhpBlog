28ba5d30285a1000c0b76cc021ba25d8a26701d4
--Angel Cabrera <soyangelcabrera@gmail.com>
2017-05-01 08:02:05 -0400

    Mejoramos las rutas con phroute

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,9 +6,25 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit2dc7520f785caf9fe4728b2771af283e
 {
+    public static $prefixLengthsPsr4 = array (
+        'P' => 
+        array (
+            'Phroute\\Phroute\\' => 16,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Phroute\\Phroute\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phroute/phroute/src/Phroute',
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit2dc7520f785caf9fe4728b2771af283e::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit2dc7520f785caf9fe4728b2771af283e::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
